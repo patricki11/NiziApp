c69fdd87e33ac25afe9ba5a217207bb94be5dffe
Samir <sy.samir.yeasin@gmail.com>
2019-11-29 08:47:41 -0800

    Initial Commit

---
+++ NiziApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NiziApp
+//
+//  Created by Samir Yeasin on 29/11/2019.
+//  Copyright © 2019 Samir Yeasin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
