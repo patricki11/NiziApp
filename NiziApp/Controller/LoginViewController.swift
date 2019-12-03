c05fac92858854aeb1ba4804fd64eb20e63ff714
Patrick Dammers <patrickdammers123@outlook.com>
2019-12-03 01:24:34 -0800

    Start nieuwe versie Auth0
    Start gemaakt met nieuwe versie van Auth0, de Auth0.plist staat nog wel op de verkeerde plaaats

--- NiziApp/Controller/LoginViewController.swift
+++ NiziApp/Controller/LoginViewController.swift
@@ -1,30 +1,80 @@
 //
 //  LoginViewController.swift
-//  NiziApp
+//  Nizi iOS Applicatie
 //
-//  Created by Samir Yeasin on 29/11/2019.
-//  Copyright © 2019 Samir Yeasin. All rights reserved.
+//  Created by Informatica Haarlem on 19-11-19.
+//  Copyright © 2019 De Mobiele Jongens. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import Auth0
 
-class LoginViewController: UIViewController {
-
+class LoginViewController : UIViewController {
+    
+    @IBOutlet weak var applicationNameLabel: UILabel!
+    @IBOutlet weak var pageTitleLabel: UILabel!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBOutlet weak var needHelpLabel: UILabel!
+    @IBOutlet weak var needHelpDescription: UILabel!
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    @IBOutlet weak var nierstichtingWebImage: UIImageView!
+    @IBOutlet weak var nierstrichtingPhoneImage: UIImageView!
+    @IBOutlet weak var nierstrichtingMailImage: UIImageView!
+    
+    @IBOutlet weak var nierstichtingWebLabel: UILabel!
+    @IBOutlet weak var nierstichtingPhoneLabel: UILabel!
+    @IBOutlet weak var nierstichtingPhoneTimeLabel: UILabel!
+    @IBOutlet weak var nierstichtingMailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        setLanguageSpecificText()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setLanguageSpecificText() {
+        applicationNameLabel.text = NSLocalizedString("ApplicationName", comment: "Nizi")
+        pageTitleLabel.text = NSLocalizedString("LoginPageTitle", comment: "")
+        usernameLabel.text = NSLocalizedString("UsernameLabel", comment: "")
+        passwordLabel.text = NSLocalizedString("PasswordLabel", comment: "")
+        needHelpLabel.text = NSLocalizedString("NeedHelp", comment: "")
+        needHelpDescription.text = NSLocalizedString("NeedHelpSubtitle", comment: "")
+        LoginButton.setTitle("Login", for: .normal)
+        nierstichtingWebLabel.text = NSLocalizedString("NierstichtingWeb", comment: "")
+        nierstichtingPhoneLabel.text = NSLocalizedString("NierstichtingPhone", comment: "")
+        nierstichtingPhoneTimeLabel.text = NSLocalizedString("NierstichtingPhoneTime", comment: "")
+        nierstichtingMailLabel.text = NSLocalizedString("NierstichtingMail", comment: "")
     }
-    */
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func Login(_ sender: Any) {
+        Auth0
+            .authentication()
+            .login(
+            usernameOrEmail: "577234@student.inholland.nl",
+            password: "TestTestTest!",
+            realm: "Username-Password-Authentication",
+            scope: "openid")
+            .start { result in
+                print(result)
+                switch result {
+                case .success(let credentials):
+                    print("Obtained credentials: \(credentials)")
+                case .failure(let error):
+                    print("Failed with \(error)")
+                }
+        }
+    }
 }
