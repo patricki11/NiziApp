bd4e7946fe0d2781fd97565371311aa4907e82ed
Patrick Dammers <patrickdammers123@outlook.com>
2020-01-06 20:36:20 +0100

    Automatic Login
    Automatisch inloggen toegevoegd voor wanneer er een token is opgeslagen

--- NiziApp/Controller/ChooseLoginViewController.swift
+++ NiziApp/Controller/ChooseLoginViewController.swift
@@ -8,18 +8,71 @@
 
 import Foundation
 import UIKit
+import SwiftKeychainWrapper
 
 class ChooseLoginViewController : UIViewController {
     
     @IBOutlet weak var loginAsPatientButton: UIButton!
     @IBOutlet weak var loginAsDietistButton: UIButton!
     
+    func checkIfLoggedIn() {
+        guard let authToken = KeychainWrapper.standard.string(forKey: "authToken") else { return }
+        
+        let isPatientAccount = KeychainWrapper.standard.string(forKey: "patientId") != nil
+        if(isPatientAccount) {
+            patientLoginToApi(authToken: authToken)
+        }
+        else {
+            dietistLoginToApi(authToken: authToken)
+        }
+    }
+    
+    func patientLoginToApi(authToken: String) {
+        NiZiAPIHelper.login(withPatientCode: authToken).responseData(completionHandler: { response in
+            guard let jsonResponse = response.data
+                else { print("temp1"); return }
+            
+            let jsonDecoder = JSONDecoder()
+            guard let patientAccount = try? jsonDecoder.decode(PatientLogin.self, from: jsonResponse )
+                else { print("temp2"); return }
+            
+            self.navigateToPatientHomepage(withPatient: patientAccount, withPatientCode: authToken)
+        })
+    }
+    
+    func dietistLoginToApi(authToken: String) {
+        NiZiAPIHelper.login(withDoctorCode: authToken).responseData(completionHandler: { response in
+            guard let jsonResponse = response.data
+            else { return }
+            
+            let jsonDecoder = JSONDecoder()
+            guard let doctorAccount = try? jsonDecoder.decode(DoctorLogin.self, from: jsonResponse )
+            else { return }
+            
+            self.navigateToPatientList(withAccount: doctorAccount)
+        })
+    }
+    
+    func navigateToPatientList(withAccount doctorAccount: DoctorLogin) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let patientListVC = storyboard.instantiateViewController(withIdentifier: "PatientListViewController") as! PatientListViewController
+        patientListVC.loggedInAccount = doctorAccount
+        self.navigationController?.pushViewController(patientListVC, animated: true)
+    }
+
+    
+    func navigateToPatientHomepage(withPatient patientAccount: PatientLogin, withPatientCode: String) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let homeVC = storyboard.instantiateViewController(withIdentifier: "MainTabBarController") as! MainTabBarController
+        homeVC.token = withPatientCode
+        self.navigationController?.pushViewController(homeVC, animated: true)
+    }
     
     override func viewDidLoad() {
-           super.viewDidLoad()
-           // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        checkIfLoggedIn()
         title = NSLocalizedString("ApplicationName", comment: "");
-           setLanguageSpecificText()
+        setLanguageSpecificText()
     }
     
     func setLanguageSpecificText() {
