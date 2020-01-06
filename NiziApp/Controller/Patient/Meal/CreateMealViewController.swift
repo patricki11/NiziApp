2df7df3e3d6a633e6581cc36ca38a64a98c2d502
Samir Yeasin <sy.samir.yeasin@gmail.com>
2020-01-06 20:13:21 +0100

    Meal works dynamically

--- NiziApp/Controller/Patient/Meal/CreateMealViewController.swift
+++ NiziApp/Controller/Patient/Meal/CreateMealViewController.swift
@@ -13,15 +13,28 @@ import Kingfisher
 class CreateMealViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var Mealfoodlist : [Food] = []
+    var kcal : Double = 0.0
+    var fiberMeal : Double = 0.0
+    var vochtMeal : Double = 0.0
+    var pottassiumMeal : Double = 0.0
+    var sodiumMeal : Double = 0.0
+    var proteinMeal : Double = 0.0
+    
     
     @IBOutlet weak var NameLabel: UILabel!
     @IBOutlet weak var NameText: UITextField!
     @IBOutlet weak var MealProducts: UITableView!
+    @IBOutlet weak var KcalLabel: UILabel!
+    @IBOutlet weak var FiberLabel: UILabel!
+    @IBOutlet weak var ProteinLabel: UILabel!
+    @IBOutlet weak var VochtLabel: UILabel!
+    @IBOutlet weak var SodiumLabel: UILabel!
+    @IBOutlet weak var PotassiumLabel: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        calculateDietary()
         // Do any additional setup after loading the view.
     }
     
@@ -34,6 +47,7 @@ class CreateMealViewController: UIViewController, UITableViewDataSource, UITable
     
     override func viewDidAppear(_ animated: Bool){
         MealProducts?.reloadData()
+        
     }
     
     
@@ -64,7 +78,10 @@ class CreateMealViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func addMeal() {
-        let meal = self.createNewMealObject(mealId: 4, name: NameText.text!, patientId: 57, kcal: 10.0, fiber: 10.0, calcium: 10.0, sodium: 10.0, portionSize: 1.0, weightUnit: "gram", picture: "https://image.flaticon.com/icons/png/512/45/45332.png", protein: 10.0)
+        
+        
+        
+        let meal = self.createNewMealObject(mealId: 4, name: NameText.text!, patientId: 57, kcal: kcal, fiber: fiberMeal, calcium: pottassiumMeal, sodium: sodiumMeal, portionSize: 1.0, weightUnit: "gram", picture: "https://image.flaticon.com/icons/png/512/45/45332.png", protein: proteinMeal)
         
         NiZiAPIHelper.addMeal(withDetails: meal, forPatient: 57, authenticationCode: KeychainWrapper.standard.string(forKey: "authToken")!).responseData(completionHandler: { response in
             // TODO: Melden aan patient dat de maaltijd is toegevoegd.
@@ -76,6 +93,46 @@ class CreateMealViewController: UIViewController, UITableViewDataSource, UITable
         let meal : Meal = Meal(
             mealId: mealId, name: name, patientId: patientId, kCal: kcal, protein: protein, fiber: fiber, calcium: calcium, sodium: sodium, portionSize: portionSize, weightUnit: weightUnit, picture: picture)
         return meal
+    }
+    
+    func calculateDietary(){
+        
+        if(Mealfoodlist.count > 0){
+            for food in Mealfoodlist {
+                kcal += food.kCal
+                fiberMeal += food.fiber
+                vochtMeal += 0.0
+                pottassiumMeal += food.calcium
+                proteinMeal +=  food.protein
+                sodiumMeal += food.sodium
+                
+            }
+            let kcalText:String = String(format:"%.1f", kcal)
+            KcalLabel.text = kcalText
+            
+            let fiberText:String = String(format:"%.1f", fiberMeal)
+            FiberLabel.text = fiberText
+            
+            let vochtText:String = String(format:"%.1f", vochtMeal)
+            VochtLabel.text = vochtText
+            
+            let pottassiumText:String = String(format:"%.1f", pottassiumMeal)
+            PotassiumLabel.text = pottassiumText
+            
+            let proteinText:String = String(format:"%.1f", proteinMeal)
+            ProteinLabel.text = proteinText
+            
+            let sodiumText:String = String(format:"%.1f", sodiumMeal)
+            SodiumLabel.text = sodiumText
+        }
+        else{
+            KcalLabel.text = "0.0"
+            FiberLabel.text = "0.0"
+            VochtLabel.text = "0.0"
+            PotassiumLabel.text = "0.0"
+            ProteinLabel.text = "0.0"
+            SodiumLabel.text = "0.0"
+        }
     }
     
 }
