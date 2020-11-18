ac14527199bdc41b576da6667837298a58f643a8
Samir Yeasin <sy.samir.yeasin@gmail.com>
2020-11-18 16:22:12 +0100

    Foodmeal class JSON method
    Added Json Function to Foodmeal class
    
    Co-Authored-By: SamirYeasin1995 <18437963+SamirYeasin1995@users.noreply.github.com>

--- NiziApp/Model/v2_API/newFoodMealComponent.swift
+++ NiziApp/Model/v2_API/newFoodMealComponent.swift
@@ -9,40 +9,55 @@
 import Foundation
 
 class newFoodMealComponent : Codable {
-    var id : Int? = 0;
-    var name : String? = ""
+    var id          : Int?    = 0;
+    var name        : String? = ""
     var description : String? = ""
-    var kcal : Float? = 0.0
-    var protein : Float? = 0.0
-    var potassium : Float? = 0.0
-    var sodium : Float? = 0.0
-    var water : Float? = 0.0
-    var fiber : Float? = 0.0
-    var portionSize : Float? = 0.0
+    var kcal        : Float?  = 0.0
+    var protein     : Float?  = 0.0
+    var potassium   : Float?  = 0.0
+    var sodium      : Float?  = 0.0
+    var water       : Float?  = 0.0
+    var fiber       : Float?  = 0.0
+    var portionSize : Float?  = 0.0
     
     init(id : Int?, name: String?, description : String?, kcal : Float?,protein : Float?, potassium : Float?, sodium: Float?, water : Float?, fiber: Float?, portionSize : Float?){
-        self.id = id
-        self.name = name
+        self.id          = id
+        self.name        = name
         self.description = description
-        self.kcal = kcal
-        self.protein = protein
-        self.potassium = potassium
-        self.sodium = sodium
-        self.water = water
-        self.fiber = fiber
+        self.kcal        = kcal
+        self.protein     = protein
+        self.potassium   = potassium
+        self.sodium      = sodium
+        self.water       = water
+        self.fiber       = fiber
         self.portionSize = portionSize
     }
     
     enum CodingKeys : String, CodingKey {
-        case id = "id"
-        case name = "name"
+        case id          = "id"
+        case name        = "name"
         case description = "description"
-        case kcal = "kcal"
-        case protein = "protein"
-        case potassium = "potassium"
-        case sodium = "sodium"
-        case water = "water"
-        case fiber = "fiber"
+        case kcal        = "kcal"
+        case protein     = "protein"
+        case potassium   = "potassium"
+        case sodium      = "sodium"
+        case water       = "water"
+        case fiber       = "fiber"
         case portionSize = "portion_size"
+    }
+    
+    func toJSON() -> [String:Any]{
+        return [
+            "id"            : id as Any,
+            "name"          : name as Any,
+            "description"   : description as Any,
+            "kcal"          : kcal as Any,
+            "protein"       : protein as Any,
+            "potassium"     : potassium as Any,
+            "sodium"        : sodium as Any,
+            "water"         : water as Any,
+            "fiber"         : fiber as Any,
+            "portion_size"  : portionSize as Any
+        ]
     }
 }
