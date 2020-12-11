7f0e5b4fc10ff3e4539e425729c85767f14e647e
Patrick Dammers <patrickdammers123@outlook.com>
2020-12-11 13:18:57 +0100

    Changed Size of Chart

--- NiziApp/Controller/CircularProgressView.swift
+++ NiziApp/Controller/CircularProgressView.swift
@@ -25,16 +25,16 @@ private var progressLayer = CAShapeLayer()
         createCircularPath()
     }
     func createCircularPath() {
-        let circularPath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: 20, startAngle: -.pi / 2, endAngle: 3 * .pi / 2, clockwise: true)
+        let circularPath = UIBezierPath(arcCenter: CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0), radius: 40, startAngle: -.pi / 2, endAngle: 3 * .pi / 2, clockwise: true)
         circleLayer.path = circularPath.cgPath
         circleLayer.fillColor = UIColor.clear.cgColor
         circleLayer.lineCap = .round
-        circleLayer.lineWidth = 10.0
+        circleLayer.lineWidth = 15.0
         circleLayer.strokeColor = UIColor.black.cgColor
         progressLayer.path = circularPath.cgPath
         progressLayer.fillColor = UIColor.clear.cgColor
         progressLayer.lineCap = .round
-        progressLayer.lineWidth = 5.0
+        progressLayer.lineWidth = 9.0
         progressLayer.strokeEnd = 0
         progressLayer.strokeColor = UIColor.red.cgColor
         layer.addSublayer(circleLayer)
