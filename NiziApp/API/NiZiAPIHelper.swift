8f9df1745c43b1d9f429a41eaff8ebb7b57be6e0
Samir Yeasin <sy.samir.yeasin@gmail.com>
2020-12-11 14:20:54 +0100

    Add Create Meal API call

--- NiziApp/API/NiZiAPIHelper.swift
+++ NiziApp/API/NiZiAPIHelper.swift
@@ -12,24 +12,24 @@ import Alamofire
 class NiZiAPIHelper {
     
     static let baseUrl = "http://104.45.16.76:1337/"
-
+    
     // LOGIN //
     static func login(withUsername username: String, andPassword password: String) -> DataRequest {
         let apiMethod = "auth/local"
         var parameters =
-        [
-            "identifier": username,
-            "password": password
-        ]
+            [
+                "identifier": username,
+                "password": password
+            ]
         
         return AF.request(baseUrl + apiMethod, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: nil)
     }
     
     static func login(withToken token: String) ->
-        DataRequest {
+    DataRequest {
         let apiMethod = "users/me"
         var header = HTTPHeaders(["Authorization" : "Bearer \(token)"])
-            
+        
         return AF.request(baseUrl + apiMethod, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: header)
     }
     // LOGIN //
@@ -54,26 +54,26 @@ class NiZiAPIHelper {
     }
     
     static func addNewConsumption(withToken token : String, withDetails consumption: NewConsumptionModel) -> DataRequest {
-         let apiMethod = "consumptions"
-         let header : HTTPHeaders = HTTPHeaders(["Authorization" : "Bearer \(token)"])
-         let parameters =
-        [
-            "amount": consumption.amount, "date": consumption.date, "meal_time": consumption.mealTime, "patient": ["id": consumption.patient.id], "weight_unit": ["unit": consumption.weightUnit.unit, "updated_at": consumption.weightUnit.updatedAt, "id": consumption.weightUnit.id, "created_at": consumption.weightUnit.createdAt, "short": consumption.weightUnit.short], "food_meal_component": ["protein": consumption.foodmealComponent.protein, "id": consumption.foodmealComponent.id, "sodium": consumption.foodmealComponent.sodium, "name": consumption.foodmealComponent.name, "kcal":  consumption.foodmealComponent.kcal, "potassium": consumption.foodmealComponent.potassium, "water": consumption.foodmealComponent.water, "description": consumption.foodmealComponent.description, "fiber": consumption.foodmealComponent.fiber, "image_url": consumption.foodmealComponent.imageUrl, "portion_size": consumption.foodmealComponent.portionSize]
-        ] as [String : Any]
-         return AF.request(baseUrl + apiMethod, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: header)
-     }
+        let apiMethod = "consumptions"
+        let header : HTTPHeaders = HTTPHeaders(["Authorization" : "Bearer \(token)"])
+        let parameters =
+            [
+                "amount": consumption.amount, "date": consumption.date, "meal_time": consumption.mealTime, "patient": ["id": consumption.patient.id], "weight_unit": ["unit": consumption.weightUnit.unit, "updated_at": consumption.weightUnit.updatedAt, "id": consumption.weightUnit.id, "created_at": consumption.weightUnit.createdAt, "short": consumption.weightUnit.short], "food_meal_component": ["protein": consumption.foodmealComponent.protein, "id": consumption.foodmealComponent.id, "sodium": consumption.foodmealComponent.sodium, "name": consumption.foodmealComponent.name, "kcal":  consumption.foodmealComponent.kcal, "potassium": consumption.foodmealComponent.potassium, "water": consumption.foodmealComponent.water, "description": consumption.foodmealComponent.description, "fiber": consumption.foodmealComponent.fiber, "image_url": consumption.foodmealComponent.imageUrl, "portion_size": consumption.foodmealComponent.portionSize]
+            ] as [String : Any]
+        return AF.request(baseUrl + apiMethod, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: header)
+    }
     
     static func patchNewConsumption(withToken token : String, withDetails consumption: NewConsumptionPatch, withConsumptionId consumptionId: Int) -> DataRequest {
-         let apiMethod = "consumptions/\(consumptionId)"
-         let header : HTTPHeaders = HTTPHeaders(["Authorization" : "Bearer \(token)"])
-         let parameters =
-        [
-            "amount": consumption.amount, "date": consumption.date, "meal_time": consumption.mealTime
-        ] as [String : Any]
+        let apiMethod = "consumptions/\(consumptionId)"
+        let header : HTTPHeaders = HTTPHeaders(["Authorization" : "Bearer \(token)"])
+        let parameters =
+            [
+                "amount": consumption.amount, "date": consumption.date, "meal_time": consumption.mealTime
+            ] as [String : Any]
         print(parameters)
         print(baseUrl+apiMethod)
-         return AF.request(baseUrl + apiMethod, method: .put, parameters: parameters, encoding: JSONEncoding.default, headers: header)
-     }
+        return AF.request(baseUrl + apiMethod, method: .put, parameters: parameters, encoding: JSONEncoding.default, headers: header)
+    }
     //NEW CONSUMPTION //
     
     //NEW SEARCH FOOD//
@@ -81,9 +81,9 @@ class NiZiAPIHelper {
         let apiMethod = "foods?name_contains=\(food)"
         let header = HTTPHeaders(["Authorization" : "Bearer \(token)"])
         let parameters =
-        [
-            "name_eq":food
-        ] as [String : Any]
+            [
+                "name_eq":food
+            ] as [String : Any]
         
         return AF.request(baseUrl + apiMethod, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: header)
     }
@@ -97,16 +97,16 @@ class NiZiAPIHelper {
     }
     
     static func addMyFood(withToken token : String, withPatientId patientId: [Int], withFoodId foodId: Int) -> DataRequest {
-         let apiMethod = "My-Foods"
-         let header : HTTPHeaders = HTTPHeaders(["Authorization" : "Bearer \(token)"])
-         let parameters =
-        [
-            "food": foodId, "patients_ids": patientId
-        ] as [String : Any]
+        let apiMethod = "My-Foods"
+        let header : HTTPHeaders = HTTPHeaders(["Authorization" : "Bearer \(token)"])
+        let parameters =
+            [
+                "food": foodId, "patients_ids": patientId
+            ] as [String : Any]
         print(parameters)
         print(baseUrl+apiMethod)
-         return AF.request(baseUrl + apiMethod, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: header)
-     }
+        return AF.request(baseUrl + apiMethod, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: header)
+    }
     // NEW FAVORITE FOOD //
     
     //GET CONVERSATIONS //
@@ -132,18 +132,18 @@ class NiZiAPIHelper {
         let header : HTTPHeaders = HTTPHeaders(["Authorization" : "Bearer \(authenticationCode)"])
         return AF.request(baseUrl + apiMethod, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: header)
     }
-
+    
     static func getDoctor(byId doctorId: Int, authenticationCode: String) -> DataRequest {
         let apiMethod = "v1/doctor/\(doctorId)"
         let header : HTTPHeaders = HTTPHeaders(["Authorization" : "Bearer \(authenticationCode)"])
         return AF.request(baseUrl + apiMethod, method: .post, parameters: nil, encoding: JSONEncoding.default, headers: header)
     }
-
+    
     static func getPatients(forDoctor doctorId: Int, withAuthorization authorizationToken: String) -> DataRequest {
         let apiMethod = "patients?doctor.id_eq=\(doctorId)"
         let header : HTTPHeaders = HTTPHeaders(["Authorization" : "Bearer \(authorizationToken)"])
         return AF.request(baseUrl + apiMethod, method: .get, headers: header)
-
+        
     }
     // DOCTORS //
     
@@ -198,8 +198,18 @@ class NiZiAPIHelper {
         return AF.request(baseUrl + apiMethod, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: header)
     }
     
+    static func createMeal(withToken token : String, withDetails consumption: NewMeal) -> DataRequest {
+        let apiMethod = "Meals"
+        let header : HTTPHeaders = HTTPHeaders(["Authorization" : "Bearer \(token)"])
+        let parameters =
+            [
+                "patient": ["id": consumption.patient.id], "weight_unit": [ "id": consumption.weightUnit.id], "food_meal_component": ["protein": consumption.foodMealComponent.protein, "id": consumption.foodMealComponent.id, "sodium": consumption.foodMealComponent.sodium, "name": consumption.foodMealComponent.name, "kcal":  consumption.foodMealComponent.kcal, "potassium": consumption.foodMealComponent.potassium, "water": consumption.foodMealComponent.water, "description": consumption.foodMealComponent.description, "fiber": consumption.foodMealComponent.fiber, "image_url": consumption.foodMealComponent.imageUrl, "portion_size": consumption.foodMealComponent.portionSize]
+            ] as [String : Any]
+        return AF.request(baseUrl + apiMethod, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: header)
+    }
+    
     // NEW MEAL //
-            
+    
     // DIETARY MANAGEMENT //
     static func getDietaryManagement(forDiet dietId: Int, authenticationCode: String) -> DataRequest {
         let apiMethod = "dietary-managements?patient.id_eq=\(dietId)&is_active=true"
