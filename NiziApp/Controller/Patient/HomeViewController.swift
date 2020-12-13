262fde94b399ad1f9c9ebd30f04effb415d7e847
Samir Yeasin <sy.samir.yeasin@gmail.com>
2020-12-13 09:46:52 +0100

    W.I.P Home

---
+++ NiziApp/Controller/Patient/HomeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  HomeViewController.swift
+//  NiziApp
+//
+//  Created by Samir Yeasin on 13/12/2020.
+//  Copyright © 2020 Samir Yeasin. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var guidelineTableView: UITableView!
+    @IBOutlet weak var dayOverviewLabel: UILabel!
+    @IBOutlet weak var currentWeekLabel: UILabel!
+    
+    @IBAction func getPreviousWeek(_ sender: Any) {
+    }
+    
+    @IBAction func getNextWeek(_ sender: Any) {
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+
+}
